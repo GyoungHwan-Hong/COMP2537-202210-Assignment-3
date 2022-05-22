dfb00fc533ab511c86cf1593646ba5dd341d8a12
GyoungHwan-Hong <hongkh5218@gmail.com>
2022-05-22 08:12:16 -0700

    Adds cookie-parser module, add db connection information,

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
 const app = express()
+
 const https = require('https')
+
 const bodyparser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
 const mongoose = require('mongoose');
 const { ppid } = require('process');
 
@@ -13,8 +17,18 @@ var session = require('express-session')
 
 app.set('view engine', 'ejs');
 
-
 app.use(express.static('./public'));
+
+//Connect Database...
+const dbAddress = "mongodb+srv://hongkh5218:recify5218@recifycluster.w6cp9.mongodb.net/recify?retryWrites=true&w=majority";
+
+mongoose
+    .connect(dbAddress, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
 
 
 app.listen(process.env.PORT || 5000, function (err) {
@@ -58,8 +72,7 @@ app.get('/userprofile/', function (req, res, next) {
   res.send("Something for userprofile.")
 })
 
-mongoose.connect("mongodb://localhost:27017/timelineDB",
-  { useNewUrlParser: true, useUnifiedTopology: true });
+
 const eventSchema = new mongoose.Schema({
   text: String,
   hits: Number,
