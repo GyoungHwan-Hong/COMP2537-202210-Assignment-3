cc00eec4d81a87ce901b2b0dff3898820f29c217
GyoungHwan-Hong <hongkh5218@gmail.com>
2022-05-29 23:47:08 -0700

    Implement dynamic grid.

--- public/pokegame.js
+++ public/pokegame.js
@@ -3,6 +3,37 @@ hasFlippedCard = false;
 firstCard = undefined
 secondCard = undefined
 
+to_add = ''
+
+function startGame() {
+    let gridNumber = $("#numberofgrid").val();
+    console.log(gridNumber);
+    to_add = ``
+
+    to_add += `<div id="game_grid">`
+
+    let widthValue = 100/gridNumber;
+    console.log(widthValue);
+
+    for (let i = 0 ; i < gridNumber; i++) {
+        console.log(i);
+        for (let j = 0; j < gridNumber; j++) {
+            to_add += `<div class="card" style="width:` + widthValue +`%">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"
+                class="front_face" alt="">
+            <img src="https://picsum.photos/id/237/100/125" class="back_face" alt="">
+        </div>`
+        }
+    }
+    to_add += ` </div>`
+
+    $("gamebody").html(to_add)
+
+    $(document).ready(setup)
+}
+
+
+
 
 function x() {
     $(this).toggleClass("flip")
